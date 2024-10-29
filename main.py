d6ad724b73bc2c7656f5e28d9f7e35ddf475c7c0
Reyhan Dany A.H. Mohammad <121990861+ree2507@users.noreply.github.com>
2024-10-29 11:19:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # Nama: Reyhan Dany_F55123095
 # Kelas: TI C
 
-# Daftar data yang akan dicari
 a = [2, 2, 1, 3, 5, 5, 5, 7, 2, 10]
 
-# Variabel yang ingin dicari
+# Variabel yang ingin dicari, jika x = 2, maka index akan muncul sebanyak 3 kali pada index 0, 1, dan 8 pada list
 x = 2
 
 # Mencari jumlah kemunculan dan indeks dari x dalam list a
 jumlah_x = a.count(x)
 indeks_x = [index for index, value in enumerate(a) if value == x]
 
-# Menampilkan hasil
 print(f"Variabel x = {jumlah_x} index {', '.join(map(str, indeks_x))}")
